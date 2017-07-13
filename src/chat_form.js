fe7d8ea1cc4fbc332b5075cd92ddf5b52cba636c
Liren <liren90@gmail.com>
2017-07-14 00:05:09 +0800

    Fix chat form

--- src/chat_form.js
+++ src/chat_form.js
@@ -3,22 +3,28 @@ import React from 'react'
 export default class ChatForm extends React.Component {
   render() {
     return(
-      <div className="input-group">
-        <span className="input-group-btn">
+      <form>
+        <div className="input-group">
+          <span className="input-group-btn">
+            <input 
+              className="form-control"
+              type="text"
+              placeholder="Name"
+              style={{width: '80px', textAlign: 'center'}} />
+          </span>
+          <div className="input-group-addon">says: </div>
           <input 
             className="form-control"
-            type="text"
-            placeholder="Name"
-            style={{width: '80px', textAlign: 'center'}} />
-        </span>
-        <div className="input-group-addon">says: </div>
-        <input 
-          className="form-control"
-          type="text" />
-        <span className="input-group-btn">
-          <button className="btn btn-default" type="button">Send</button>
-        </span>
-      </div>
+            type="text" />
+          <span className="input-group-btn">
+            <input
+              className="btn btn-default"
+              type="submit"
+              value="Send"
+            />
+          </span>
+        </div>
+      </form>
     )
   }
 }
