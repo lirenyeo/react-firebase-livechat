12a7e31117b23125d325a7c9901a8188571c157d
Liren <liren90@gmail.com>
2017-07-13 11:34:03 +0800

    Better formatting

--- src/chat_form.js
+++ src/chat_form.js
@@ -5,10 +5,16 @@ export default class ChatForm extends React.Component {
     return(
       <div className="input-group">
         <span className="input-group-btn">
-          <input placeholder="Name" type="text" className="form-control" style={{width: '80px', textAlign: 'center'}}/>
+          <input 
+            className="form-control"
+            type="text"
+            placeholder="Name"
+            style={{width: '80px', textAlign: 'center'}} />
         </span>
         <div className="input-group-addon">says: </div>
-        <input type="text" className="form-control" />
+        <input 
+          className="form-control"
+          type="text" />
         <span className="input-group-btn">
           <button className="btn btn-default" type="button">Send</button>
         </span>
